4d3dd82801374d0ef33b614a386451e8da883b51
Gunasekhar Matamalam <gunasekhar.matamalam@suse.com>
2023-11-01 14:38:33 -0700

    Revert to the earlier GA and added assetPrefix to next.config.js
    Signed-off-by: Gunasekhar Matamalam <gunasekhar.matamalam@suse.com>

--- next.config.js
+++ next.config.js
@@ -3,6 +3,6 @@ module.exports = {
   images: {
     unoptimized: true
   },
-  //assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH,
+  assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH ? process.env.NEXT_PUBLIC_BASE_PATH : '',
   basePath: process.env.NEXT_PUBLIC_BASE_PATH ? process.env.NEXT_PUBLIC_BASE_PATH : '',
 }
