2585dcb1a68cb63bb53b5e220f66a0e9b693d82c
Gunasekhar Matamalam <gunasekhar.matamalam@suse.com>
2023-11-01 16:01:57 -0700

    Updated basePath,assetPrefix to add reponame
    Signed-off-by: Gunasekhar Matamalam <gunasekhar.matamalam@suse.com>

--- next.config.js
+++ next.config.js
@@ -3,6 +3,6 @@ module.exports = {
   images: {
     unoptimized: true
   },
-  // assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH ? process.env.NEXT_PUBLIC_BASE_PATH : '',
-  basePath: process.env.NEXT_PUBLIC_BASE_PATH ? process.env.NEXT_PUBLIC_BASE_PATH : 'rancherdesktop.io',
+  assetPrefix: process.env.NEXT_PUBLIC_DEVELOPMENT ? '' : '/rancherdesktop.io/',
+  basePath: process.env.NEXT_PUBLIC_DEVELOPMENT ? '' : '/rancherdesktop.io',
 }
