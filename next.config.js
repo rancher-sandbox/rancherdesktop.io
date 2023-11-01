a3a79eb5e43f24208cedec2e87067da2330a4abb
Gunasekhar Matamalam <gunasekhar.matamalam@suse.com>
2023-11-01 10:27:43 -0700

    Updated next.config.js to add basePath as it's required for prod deployment
    Signed-off-by: Gunasekhar Matamalam <gunasekhar.matamalam@suse.com>

--- next.config.js
+++ next.config.js
@@ -1,7 +1,9 @@
 module.exports = {
   reactStrictMode: true,
   swcMinify: true,
-  // if hosted in Github pages, since the format is user.github.io/REPO_NAME and Next.js expects the assets to live under the main path /, we need to rewrite it with the repository name.
-  // assetPrefix: process.env.NEXT_PUBLIC_DEVELOPMENT ? '' : '/project-template/',
-  // basePath: process.env.NEXT_PUBLIC_DEVELOPMENT ? '' : '/project-template',
+  images: {
+    unoptimized: true
+  },
+  //assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH,
+  basePath: process.env.NEXT_PUBLIC_BASE_PATH ? process.env.NEXT_PUBLIC_BASE_PATH : '',
 }
