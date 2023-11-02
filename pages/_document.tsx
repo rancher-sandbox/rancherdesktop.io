bf41756524d69c5641527b99c46c85a0ec8c277b
Gunasekhar Matamalam <gunasekhar.matamalam@suse.com>
2023-11-01 23:19:51 -0700

    Fixed issue with loading favicon
    Signed-off-by: Gunasekhar Matamalam <gunasekhar.matamalam@suse.com>

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,5 +1,6 @@
 /* eslint-disable @next/next/no-page-custom-font */
 import { Head, Html, Main, NextScript } from 'next/document'
+import { PREFIX } from '@/utils/constants'
 
 export default function Document() {
   return (
@@ -9,8 +10,8 @@ export default function Document() {
         <link rel='preconnect' href='https://fonts.gstatic.com' />
         <link href='https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;700&display=swap' rel='stylesheet' />
         <link rel='apple-touch-icon' sizes='120x120' href='/apple-touch-icon.png' />
-        <link rel='icon' type='image/png' sizes='32x32' href='/favicon-32x32.png' />
-        <link rel='icon' type='image/png' sizes='16x16' href='/favicon-16x16.png' />
+        <link rel='icon' type='image/png' sizes='32x32' href={`${PREFIX}/favicon-32x32.png`} />
+        <link rel='icon' type='image/png' sizes='16x16' href={`${PREFIX}/favicon-16x16.png`} />
         <link rel='manifest' href='/site.webmanifest' />
         <meta name='msapplication-TileColor' content='#da532c' />
         <meta name='theme-color' content='#ffffff'></meta>
